3aa0459479c145d2ccd16732919faa97823ea2ca
REDsky96 <mmnnknm.lk@gmail.com>
2018-10-15 17:52:24 +0900

    2018.10.15

---
+++ java1/src/java1/HelloWorld.java
@@ -0,0 +1,12 @@
+package java1;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		String[] name = {"田中","伊藤","鈴木"};
+		name[2]="加藤";
+		System.out.println(name[2]);
+
+	}
+
+}
