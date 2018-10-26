2e8f597cb390ba86e669b4e1d46bd853c9a32806
REDsky96 <mmnnknm.lk@gmail.com>
2018-10-26 17:46:43 +0900

    18/10/26

--- java練習２/src/java練習２/Java_practice.java
+++ java練習２/src/java練習２/Java_practice.java
@@ -1,29 +1,36 @@
 package java練習２;
 
-public class Java_practice {
-	private String name = "";
-	private int age = 0;
+public class Java_practice{
+  private String name = null;
+  private int age = 0;
 
-	public Java_practice(){}
+  public Java_practice(String name, int age){
+	  this.name=name;
+	  this.age=age;
+  }
 
-	public Java_practice(String name , int age){
-		this.name=name;
-		this.age=age;
-	}
+  public Java_practice(int age){
+	  this.name="名前なし";
+	  this.age=age;
+  }
 
-	public String getName(){
-		return this.name;
-	}
+  public String getName(){
+	  return this.name;
+  }
 
-	public void setName(String name){
-		this.name=name;
-	}
+  public void setName(String name){
+	  this.name=name;
+  }
 
-	public int getAge(){
-		return this.age;
-	}
+  public int getAge(){
+	  return this.age;
+  }
 
-	public void setAge(int age){
-		this.age=age;
-	}
+  public void setAge(int age){
+	  this.age=age;
+  }
+
+  public void talk(){
+	  System.out.println(this.name+"が話す");
+  }
 }
--- java練習２/src/java練習２/Test.java
+++ java練習２/src/java練習２/Test.java
@@ -1,12 +1,14 @@
 package java練習２;
 
 public class Test {
+  public static void main(String[] args){
+	  Java_practice sato = new Java_practice("佐藤",20);
+	  System.out.println(sato.getName());
+	  System.out.println(sato.getAge());
+	  sato.talk();
 
-	public static void main(String[] args){
+	  sato.setName("hanako");
+	  System.out.println(sato.getName());
 
-		Java_practice taro = new Java_practice("山田太郎",23);
-			System.out.println(taro.getName());
-			System.out.println(taro.getAge());
-    }
-
+  }
 }
