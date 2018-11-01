aa21e8aedf24d5870261b55b0a223471caba6b8a
REDsky96 <mmnnknm.lk@gmail.com>
2018-11-01 17:55:56 +0900

    18/11/31

--- WebProj5/src/com/internousdev/webproj5/action/InquiryCompleteAction.java
+++ WebProj5/src/com/internousdev/webproj5/action/InquiryCompleteAction.java
@@ -5,13 +5,14 @@ import java.util.List;
 import java.util.Map;
 
 import org.apache.struts2.interceptor.SessionAware;
+
 import com.internousdev.webproj5.dao.InquiryCompleteDAO;
 import com.internousdev.webproj5.dto.InquiryDTO;
 import com.opensymphony.xwork2.ActionSupport;
 
 
 
-public class InquiryCompleteAction extends ActionSupport{
+public class InquiryCompleteAction extends ActionSupport implements SessionAware{
 
 	private String name;
 	private String qtype;
@@ -30,5 +31,37 @@ public class InquiryCompleteAction extends ActionSupport{
 			ret = SUCCESS;
 		}
 		return ret;
+	}
+
+	public String getName(){
+		return name;
+	}
+
+	public void setName(String name){
+		this.name = name;
+	}
+
+	public String getQtype(){
+		return qtype;
+	}
+
+	public void setQtype(String qtype){
+		this.qtype = qtype;
+	}
+
+	public String getBody(){
+		return body;
+	}
+
+	public void setBody(String body){
+		this.body = body;
+	}
+
+	public Map<String, Object> getSession(){
+		return session;
+	}
+
+	public void setSession(Map<String, Object> session){
+		this.session = session;
 	}
 }
